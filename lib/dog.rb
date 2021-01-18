ae2dcdf2eabc233c3357d9bc7c5a3d6afb476535
Jason Herrera <baytownjason@yahoo.com>
2021-01-18 17:07:18 -0600

    commit tree 5a0dd8d632232fbfebd2387b55b782dbed4848c1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
+require 'pry'
 # Add your code here
+class Dog
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    
+        @self = self
+        save
+    end
+    def self.all
+        @@all
+    end
+    def self.print_all
+        @@all.each do |instance|
+            #binding.pry
+            puts instance.name
+        end
+    end
+    def self.clear_all
+        @@all = []
+    end
+    def save
+        @@all << @self
+    end
+    def name
+        @name
+    end
+end
+
+# chiloh = Dog.new("Xhiloh")
+# bane = Dog.new("Bane")
+# p Dog.all
+# p Dog.print_all
